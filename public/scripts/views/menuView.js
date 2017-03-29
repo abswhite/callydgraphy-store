142919f6ba13c1d497996b7d90e1e61533e6cd3f
Abigail White <abigailwhite213@gmail.com>
2017-03-28 21:29:20 -0400

    issue centering nav icons

--- public/scripts/views/menuView.js
+++ public/scripts/views/menuView.js
@@ -4,16 +4,20 @@
 
   let click = function() {
   $('#menu_icon').click(function() {
-      $('nav ul').show();
+      $('#nav-mobile ul').show();
       console.log('#menu_icon');
   })
 }
 
 let clickNav = function() {
-  $('nav ul li').click(function() {
-    $('nav ul').hide();
+  $('#nav-mobile ul li').click(function() {
+    $('#nav-mobile ul').hide();
     console.log('Menu ul hide');
+  });
 
+  $('#cart_icon').click(function() {
+    console.log('menu');
+    $('#nav-mobile ul').hide();
   });
 };
 
