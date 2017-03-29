cc1cdd75b9410db7526f4800eabc6912bdcb8522
Abigail White <abigailwhite213@gmail.com>
2017-03-28 23:23:07 -0400

    remove console.logs

--- public/scripts/views/menuView.js
+++ public/scripts/views/menuView.js
@@ -5,18 +5,15 @@
   let click = function() {
   $('#menu_icon').click(function() {
       $('#nav-mobile ul').show();
-      console.log('#menu_icon');
   })
 }
 
 let clickNav = function() {
   $('#nav-mobile ul li').click(function() {
     $('#nav-mobile ul').hide();
-    console.log('Menu ul hide');
   });
 
   $('#cart_icon').click(function() {
-    console.log('menu');
     $('#nav-mobile ul').hide();
   });
 };
