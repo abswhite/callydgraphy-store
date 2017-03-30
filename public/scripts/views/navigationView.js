aa73537b2e03c805fa9e6b495133155310701fce
Abigail White <abigailwhite213@gmail.com>
2017-03-29 20:58:12 -0400

    nav changes responsive to scrolling

--- public/scripts/views/navigationView.js
+++ public/scripts/views/navigationView.js
@@ -2,28 +2,28 @@
 
 $(document).ready(function() {
 
-  // $('#cart_icon1').hide();
-
   $(window).scroll(function () {
       //if you hard code, then use console
       //.log to determine when you want the
       //nav bar to stick.
       console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 60) {
+    if ($(window).scrollTop() >= 80) {
       $('.top-bar').addClass('navbar-fixed');
       $('.top-bar h1').addClass('h1-scroll');
       $('.butterfly').addClass('butterfly-scroll');
-      $('#cart_icon1').show();
-      $('#cart_icon').hide();
+      $('.subtitle').addClass('subtitle-margin');
+      $('.cart_icon').addClass('cart_icon1');
+      $('.cart_icon').removeClass('cart_icon2');
 
     }
 
-    if ($(window).scrollTop() < 60) {
+    if ($(window).scrollTop() < 80) {
       $('.top-bar').removeClass('navbar-fixed');
       $('.top-bar h1').removeClass('h1-scroll');
       $('.butterfly').removeClass('butterfly-scroll');
-      $('#cart_icon1').hide();
-      $('#cart_icon').hide();
+      $('.subtitle').removeClass('subtitle-margin');
+      $('.cart_icon').removeClass('cart_icon1');
+      $('.cart_icon').addClass('cart_icon2');
 
     }
   });
