dbbbcb720291ddc75b6c0278b9170ea85f4215bd
Abigail White <abigailwhite213@gmail.com>
2017-03-30 22:42:04 -0400

    stick footer

--- public/scripts/views/homeGallery.js
+++ public/scripts/views/homeGallery.js
@@ -1,20 +1,20 @@
-'use strict';
-
-(function(module) {
-
-    let slideIndex = 0;
-    carousel();
-
-    function carousel() {
-      let i;
-      let x = document.getElementsByClassName("mySlides");
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex > x.length) {slideIndex = 1}
-      x[slideIndex-1].style.display = "block";
-      setTimeout(carousel, 3000);
-
-  };
-})(window);
+// 'use strict';
+//
+// (function(module) {
+//
+//     let slideIndex = 0;
+//     carousel();
+//
+//     function carousel() {
+//       let i;
+//       let x = document.getElementsByClassName("mySlides");
+//       for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//       }
+//       slideIndex++;
+//       if (slideIndex > x.length) {slideIndex = 1}
+//       x[slideIndex-1].style.display = "block";
+//       setTimeout(carousel, 3000);
+//
+//   };
+// })(window);
