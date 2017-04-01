408ee6c40b6ea9d2cd810d51ce8c6857d83d5671
Abigail White <abigailwhite213@gmail.com>
2017-04-01 10:06:43 -0400

    gallery edit

--- public/scripts/views/homeGallery.js
+++ public/scripts/views/homeGallery.js
@@ -2,19 +2,24 @@
 //
 // (function(module) {
 //
-//     let slideIndex = 0;
-//     carousel();
+//   let automation = function() {
+  //   let slideIndex = 0;
+  //   carousel();
+  //
+  //   function carousel() {
+  //     let i;
+  //     let x = document.getElementsByClassName("mySlides");
+  //     for (i = 0; i < x.length; i++) {
+  //       x[i].style.display = "none";
+  //     }
+  //     slideIndex++;
+  //     if (slideIndex > x.length) {slideIndex = 1}
+  //     x[slideIndex-1].style.display = "block";
+  //     setTimeout(carousel, 3000);
+  //
+  // };
+// };
 //
-//     function carousel() {
-//       let i;
-//       let x = document.getElementsByClassName("mySlides");
-//       for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";
-//       }
-//       slideIndex++;
-//       if (slideIndex > x.length) {slideIndex = 1}
-//       x[slideIndex-1].style.display = "block";
-//       setTimeout(carousel, 3000);
 //
-//   };
+// automation();
 // })(window);
